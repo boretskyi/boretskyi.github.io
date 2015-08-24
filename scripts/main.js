da4f8cf16ec7352257cd1605240e4c09c2799adb
Bogdan Kulynych <hello@bogdankulynych.me>
2015-08-24 21:32:30 +0300

    Make navbar always sticky

--- scripts/main.js
+++ scripts/main.js
@@ -18,27 +18,27 @@ $('#name').focus(function() {
 $("img").addClass("img-responsive")
 
 // Navigation Scripts to Show Header on Scroll-Up
-// $(function() {
-//   //primary navigation slide-in effect
-//   var headerHeight = $('.navbar-custom').height();
-//   $(window).on('scroll', {
-//       previousTop: 0
-//     },
-//     function() {
-//       var currentTop = $(window).scrollTop();
-//       //check if user is scrolling up
-//       if (currentTop < this.previousTop) {
-//         //if scrolling up...
-//         if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
-//           $('.navbar-custom').addClass('is-visible');
-//         } else {
-//           $('.navbar-custom').removeClass('is-visible is-fixed');
-//         }
-//       } else {
-//         //if scrolling down...
-//         $('.navbar-custom').removeClass('is-visible');
-//         if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
-//       }
-//     this.previousTop = currentTop;
-//   });
-// });
+$(function() {
+  //primary navigation slide-in effect
+  var headerHeight = $('.navbar-custom').height();
+  $(window).on('scroll', {
+      previousTop: 0
+    },
+    function() {
+      var currentTop = $(window).scrollTop();
+      //check if user is scrolling up
+      if (currentTop < this.previousTop) {
+        //if scrolling up...
+        if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
+          $('.navbar-custom').addClass('is-visible');
+        } else {
+          $('.navbar-custom').removeClass('is-visible is-fixed');
+        }
+      } else {
+        //if scrolling down...
+        $('.navbar-custom').removeClass('is-visible');
+        if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
+      }
+    this.previousTop = currentTop;
+  });
+});
