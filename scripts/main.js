5ccaffaccc27186349dbf86f361af9d491b7d1ad
Bogdan Kulynych <hello@bogdankulynych.me>
2015-08-24 17:02:33 +0300

    Try to fix facebook buttons /w instantclick

--- scripts/main.js
+++ scripts/main.js
@@ -6,12 +6,12 @@
 
 // Tooltip Init
 $(function() {
-    $("[data-toggle='tooltip']").tooltip();
+  $("[data-toggle='tooltip']").tooltip();
 });
 
 // When clicking on Full hide fail/success boxes
 $('#name').focus(function() {
-    $('#success').html('');
+  $('#success').html('');
 });
 
 // Make all images responsive
@@ -19,26 +19,26 @@ $("img").addClass("img-responsive")
 
 // Navigation Scripts to Show Header on Scroll-Up
 $(function() {
-    //primary navigation slide-in effect
-    var headerHeight = $('.navbar-custom').height();
-    $(window).on('scroll', {
-            previousTop: 0
-        },
-        function() {
-            var currentTop = $(window).scrollTop();
-            //check if user is scrolling up
-            if (currentTop < this.previousTop) {
-                //if scrolling up...
-                if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
-                    $('.navbar-custom').addClass('is-visible');
-                } else {
-                    $('.navbar-custom').removeClass('is-visible is-fixed');
-                }
-            } else {
-                //if scrolling down...
-                $('.navbar-custom').removeClass('is-visible');
-                if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
-            }
-            this.previousTop = currentTop;
-        });
+  //primary navigation slide-in effect
+  var headerHeight = $('.navbar-custom').height();
+  $(window).on('scroll', {
+      previousTop: 0
+    },
+    function() {
+      var currentTop = $(window).scrollTop();
+      //check if user is scrolling up
+      if (currentTop < this.previousTop) {
+        //if scrolling up...
+        if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
+          $('.navbar-custom').addClass('is-visible');
+        } else {
+          $('.navbar-custom').removeClass('is-visible is-fixed');
+        }
+      } else {
+        //if scrolling down...
+        $('.navbar-custom').removeClass('is-visible');
+        if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
+      }
+    this.previousTop = currentTop;
+  });
 });
