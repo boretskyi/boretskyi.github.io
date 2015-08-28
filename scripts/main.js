1bd4e9edf8f69c6b859da73f8b51d9edcff79cfd
Bogdan Kulynych <hello@bogdankulynych.me>
2015-08-28 13:03:44 +0300

    Hopefully fix navbar finally

--- scripts/main.js
+++ scripts/main.js
@@ -1,57 +1,60 @@
  /*!
  * Based on Clean Blog v1.0.0 (http://startbootstrap.com)
- * Copyright 2014 Start Bootstrap
+ * Copyright 2014 Start Bootstrap, Bogdan Kulynych
  * Licensed under Apache 2.0 (https://github.com/IronSummitMedia/startbootstrap/blob/gh-pages/LICENSE)
  */
 
-// Tooltip Init
 $(function() {
+  // Tooltip Init
   $("[data-toggle='tooltip']").tooltip();
-});
 
-// When clicking on Full hide fail/success boxes
-$('#name').focus(function() {
-  $('#success').html('');
-});
+  // When clicking on Full hide fail/success boxes
+  $('#name').focus(function() {
+    $('#success').html('');
+  });
 
-// Make all images responsive
-$("img").addClass("img-responsive");
+  // Make all images responsive
+  $("img").addClass("img-responsive");
+});
 
 // Navigation Scripts to Show Header on Scroll-Up
 $(function() {
   //primary navigation slide-in effect
   var WIDTH_THRESHOLD = 768;
+  var $window = $(window),
+      $navbar = $('.navbar-custom');
 
-  var headerHeight = $('.navbar-custom').height();
-  $(window).on('scroll', {
+  var headerHeight = $navbar.height();
+  $window.on('load resize', function() {
+    var currentTop = $window.scrollTop();
+    if (currentTop === 0 && $window.width() <= WIDTH_THRESHOLD) {
+      $navbar.addClass('is-visible is-fixed');
+    } else if ($window.width() > WIDTH_THRESHOLD) {
+      $navbar.removeClass('is-visible is-fixed');
+    }
+  });
+  $window.on('scroll', {
       previousTop: 0
     },
     function() {
-      var currentTop = $(window).scrollTop();
-      if (currentTop === 0) {
-        if ($(window).width() > WIDTH_THRESHOLD) {
-          $('.navbar-custom').removeClass('is-visible is-fixed');
-        } else {
-          $('.navbar-custom').removeClass('is-fixed');
-        }
-      } else {
+      var currentTop = $window.scrollTop();
       //check if user is scrolling up
       if (currentTop <= this.previousTop) {
         //if scrolling up...
-        if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
-          $('.navbar-custom').addClass('is-visible');
+        if (currentTop > 0 && $navbar.hasClass('is-fixed')) {
+          $navbar.addClass('is-visible');
         } else {
-          if ($(window).width() > WIDTH_THRESHOLD) {
-            $('.navbar-custom').removeClass('is-visible is-fixed');
+          if ($window.width() > WIDTH_THRESHOLD) {
+            $navbar.removeClass('is-visible is-fixed');
           }
         }
       } else {
         //if scrolling down...
-        $('.navbar-custom').removeClass('is-visible');
-        if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) {
-          $('.navbar-custom').addClass('is-fixed');
+        $navbar.removeClass('is-visible');
+        if (currentTop > headerHeight && !$navbar.hasClass('is-fixed')) {
+          $navbar.addClass('is-fixed');
         }
-      }}
-    this.previousTop = currentTop;
+      }
+      this.previousTop = currentTop;
   });
 });
